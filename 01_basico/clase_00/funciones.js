9cc47240422825e994bae81d0daa67109021b38e
nicohnavarro <nicohnavarro@gmail.com>
2019-08-26 15:53:32 -0300

    form user

--- 01_basico/clase_00/funciones.js
+++ 01_basico/clase_00/funciones.js
@@ -1,7 +1,7 @@
 function mostrar(){
-    var usr = document.getElementById("usr").value;
-    var pass= document.getElementById("pass").value;
-    if(usr=="hola" && pass=="mundo"){
+    var usuario = document.getElementById("usuario").value;
+    var password= document.getElementById("password").value;
+    if(usuario=="37354627" && password=="37354627"){
         alert("Todo esta bien");
     } else {
         alert("Usuarios y Password Equivocados");
