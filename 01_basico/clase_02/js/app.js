1b795ae3daad37cd8d9d515b1915bf7a3e58fed3
nicohnavarro <nicohnavarro@gmail.com>
2019-09-13 22:12:21 -0300

    css

--- 01_basico/clase_02/js/app.js
+++ 01_basico/clase_02/js/app.js
@@ -18,17 +18,23 @@ function Agregar() {
         if (confirm("Desea Agregar una persona? ")) {
             var texto = "<tr>";
             texto += "<td>";
-            texto += name;
+            texto += name.value;
             texto += "</td>";
             texto += "<td>";
-            texto += surname;
+            texto += surname.value;
             texto += "</td>";
             texto += "<td>";
             texto += "<a href='#'' onclick='borrar(event)'>Borrar</a>";
             texto += "</td>";
+            texto += "<td>";
+            texto += "<a href='' onclick='editar(event)'>editar</a>";
+            texto += "</td>";
+            
             texto += "</tr>";
 
             document.getElementById("tabla").innerHTML += texto;
+            //document.getElementById("name").value="";
+            //document.getElementById("surname").value="";
         }
     }
 
@@ -36,7 +42,13 @@ function Agregar() {
 
 function borrar(e){
     e.preventDefault();
-    console.log(e.target.parentNode.parentNode);
+    console.log(e);
     if(confirm("Desea borrar?"));
     e.target.parentNode.parentNode.innerHTML="";
+}
+
+function editar(e){
+    e.preventDefault();
+    var variable=document.getElementsByTagName("td")[0].innerHTML;
+    console.log(variable);
 }
